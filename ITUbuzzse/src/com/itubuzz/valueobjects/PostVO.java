fa67a3cf7f1b8b63768b685ddadec636bec3ed68
itubuzz <kavalgikaraparn100@students.itu.edu>
2016-04-04 03:56:16 -0700

    Fixed issue of Post Reply Question Ans ans Search

--- ITUbuzzse/src/com/itubuzz/valueobjects/PostVO.java
+++ ITUbuzzse/src/com/itubuzz/valueobjects/PostVO.java
@@ -5,6 +5,7 @@ public class PostVO {
 	public int post_id;
 	public String post_text;
 	public int log_user_id;
+	public String log_user_name;
 	
 	public int getPost_id() {
 		return post_id;
@@ -24,5 +25,16 @@ public class PostVO {
 	public void setLog_user_id(int log_user_id) {
 		this.log_user_id = log_user_id;
 	}
+	public String getlog_user_name() {
+		return log_user_name;
+	}
+	public void setlog_user_name(String post_user_name) {
+		this.log_user_name = post_user_name;
+	}
+	public void setPost_text(String post_text) {
+		this.post_text = post_text;
+	}
+	
+	
 	
 }
